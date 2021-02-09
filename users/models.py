4ab0413cdde5c2960630f8a196ba35b7cc4ed45e
dalghake-kelas <78718938+dalghake-kelas@users.noreply.github.com>
2021-02-09 12:17:05 +0330

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    # followers = models.
